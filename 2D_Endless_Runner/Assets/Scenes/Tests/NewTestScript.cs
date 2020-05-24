3ca7b55605a3b145d1b8e76a76a3ca6e151a3dca
Stanley Zhu <yhf7878@autuni.ac.nz>
2020-05-25 01:43:03 +1200

    adding change_colour

---
+++ 2D_Endless_Runner/Assets/Scenes/Tests/NewTestScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class damage_calculator
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void NewTestScriptSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator NewTestScriptWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+
+//         using System;
+// namespace Acme.Collections
+{
+    public class Stack
+    {
+        Entry top;
+        public void Push(object data)
+        {
+            top = new Entry(top, data);
+        }
+
+        public object Pop()
+        {
+            if (top == null)
+            {
+                throw new InvalidOperationException();
+            }
+            object result = top.data;
+            top = top.next;
+            return result;
+        }
+
+        class Entry
+        {
+            public Entry next;
+            public object data;
+            public Entry(Entry next, object data)
+            {
+                this.next = next;
+                this.data = data;
+            }
+        }
+    }
+}
+    }
+}
